4d49959ef5c37f70fecbd907325fb4b26ea451f2
gneL1 <451635157@qq.com>
2020-05-09 17:13:45 +0800

    add files

--- lib/base_widget/base_widget_index.dart
+++ lib/base_widget/base_widget_index.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/screenutil.dart';
+import 'package:flutterexperiences/utils/util_item.dart';
 
 class BaseWidgetIndex extends StatefulWidget {
   BaseWidgetIndex({ Key key }) : super(key: key);
@@ -8,6 +10,7 @@ class BaseWidgetIndex extends StatefulWidget {
 }
 
 class BaseWidgetIndexState extends State<BaseWidgetIndex> {
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,8 +19,47 @@ class BaseWidgetIndexState extends State<BaseWidgetIndex> {
       ),
       body: Column(
         children: <Widget>[
+          UtilItem(
+            icon: _text(text: 'Tv'),
+            onPressed: (){
+
+            },
+            title: 'TextView',
+            description: '文本组件',
+            isShape: true,
+          )
 
         ],
+      ),
+    );
+  }
+
+  Widget _text({String text}){
+    return ShaderMask(
+      shaderCallback: (Rect bounds){
+        return RadialGradient(
+          center: Alignment.center,
+          radius: 1,
+          tileMode: TileMode.mirror,
+          colors: <Color>[
+            Colors.lightBlueAccent,Colors.lightBlue[300]
+          ],
+        ).createShader(bounds);
+      },
+      child: Text(
+          text,
+          style: TextStyle(
+              color: Colors.white,
+              fontSize: ScreenUtil().setSp(36),
+              fontWeight: FontWeight.w500,
+              shadows: [
+                Shadow(
+                    color: Colors.grey,
+                    offset: Offset(1, 1),
+                    blurRadius: 2
+                )
+              ]
+          )
       ),
     );
   }
